c228fdb2814cbf4bb0c1f65579fe084c376f4e21
Carl <labaynecss@gmail.com>
2023-07-04 21:11:08 +0800

    merge new

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,15 +1,13 @@
-import { useContext } from "react";
-import LoginandRegisterForm from "./page/LoginandRegisteForm";
-import { UserContext } from "./UserContext";
-import Chat from "./Chat";
+import { useContext } from 'react';
+import LoginandRegisterForm from './page/LoginandRegisteForm';
+import { UserContext } from './UserContext';
+import Chat from './Chat';
 
 export default function Routes() {
   const { username } = useContext(UserContext);
-<<<<<<< HEAD
+
   console.log('user', username);
-=======
-  console.log("user", username);
->>>>>>> ccd2dd632211e5adb19caf1aef8f1fa8d3b21d60
+
   if (username) {
     return <Chat />;
   }
